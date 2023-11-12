7595b3c7357803a14fea926def5bf186d91742b2
Joao Tentis <101594270+jtentis@users.noreply.github.com>
2023-11-12 18:31:34 -0300

    fix: função desaparecer animacao

--- script.js
+++ script.js
@@ -5,9 +5,9 @@ const observer = new IntersectionObserver((entries) => {
         if (entry.isIntersecting){
             entry.target.classList.add('show');
         }
-        else{
-            entry.target.classList.remove('show')
-        }
+        // else{
+        //     entry.target.classList.remove('show')
+        // }
     });
 });
 
