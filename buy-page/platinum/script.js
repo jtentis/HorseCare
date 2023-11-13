91be872086224de4a50ed36a31c69ad7476b210c
Joao Tentis <101594270+jtentis@users.noreply.github.com>
2023-11-12 21:32:17 -0300

    feat, doc: melhorando login page e responsividade

--- buy-page/platinum/script.js
+++
@@ -1,8 +0,0 @@
-function img(anything) {
-    document.querySelector('.slide').src = anything;
-  }
-
-  function change(change) {
-    const line = document.querySelector('.home');
-    line.style.background = change;
-  }
