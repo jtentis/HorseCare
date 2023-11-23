284ca53d65095337d48a2c87d24de06f17e3a534
joao tentis <101594270+jtentis@users.noreply.github.com>
2023-11-23 19:08:33 -0300

    fix: consertando erro nas imagens buy-page

--- login-page/index.php
+++ login-page/index.php
@@ -36,7 +36,7 @@
 <html lang="en">
 
 <head>
-    <link rel="icon" href="/Imagens/Logos/LOGO-CUT-WHITE.png">
+    <link rel="icon" href="LOGO/LOGO-CUT-WHITE.png">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
